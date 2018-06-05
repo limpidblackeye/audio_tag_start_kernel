5da3f6e6992f5878fedce7d7ab2559b7e5ddbeb4
YonggeLi <limipid.eye@gmail.com>
2018-06-05 15:54:38 +0800

    debug

--- start_kernel.py
+++ start_kernel.py
@@ -569,25 +569,26 @@ def prepare_data(df, config, data_dir):
     input_length = config.audio_length
     for i, fname in enumerate(df.index):
         print(fname)
-        file_path = data_dir + fname
-        data, _ = librosa.core.load(file_path, sr=config.sampling_rate, res_type="kaiser_fast")
+        if ".wav" in fname:
+            file_path = data_dir + fname
+            data, _ = librosa.core.load(file_path, sr=config.sampling_rate, res_type="kaiser_fast")
 
-        # Random offset / Padding
-        if len(data) > input_length:
-            max_offset = len(data) - input_length
-            offset = np.random.randint(max_offset)
-            data = data[offset:(input_length+offset)]
-        else:
-            if input_length > len(data):
-                max_offset = input_length - len(data)
+            # Random offset / Padding
+            if len(data) > input_length:
+                max_offset = len(data) - input_length
                 offset = np.random.randint(max_offset)
+                data = data[offset:(input_length+offset)]
             else:
-                offset = 0
-            data = np.pad(data, (offset, input_length - len(data) - offset), "constant")
+                if input_length > len(data):
+                    max_offset = input_length - len(data)
+                    offset = np.random.randint(max_offset)
+                else:
+                    offset = 0
+                data = np.pad(data, (offset, input_length - len(data) - offset), "constant")
 
-        data = librosa.feature.mfcc(data, sr=config.sampling_rate, n_mfcc=config.n_mfcc)
-        data = np.expand_dims(data, axis=-1)
-        X[i,] = data
+            data = librosa.feature.mfcc(data, sr=config.sampling_rate, n_mfcc=config.n_mfcc)
+            data = np.expand_dims(data, axis=-1)
+            X[i,] = data
     return X
 
 
